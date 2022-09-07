809b1f80be54221b2b66e1bc04284c0270b5c1dd
Fabio Fujii <fujii.kimio.k@gmail.com>
2022-09-07 19:26:27 -0300

    changing the model to random forest

--- classifier_model/pipeline.py
+++ classifier_model/pipeline.py
@@ -1,6 +1,6 @@
 from feature_engine.encoding import WoEEncoder
 from feature_engine.imputation import CategoricalImputer, MeanMedianImputer
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
@@ -33,6 +33,6 @@ spaceship_titanic_pipeline = Pipeline(
             WoEEncoder(variables=config.model_config.cat_woe_encoding),
         ),
         ("StandardScaler", StandardScaler()),
-        ("LogisticRegression", LogisticRegression()),
+        ("LogisticRegression", RandomForestClassifier()),
     ]
 )
